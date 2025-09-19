578c2a31b3c4f32506c9416a58ab9a2a2f5fa78f
Andriy <aaandriy92@gmail.com>
2025-09-19 20:19:11 +0300

    feat(middleware): add error handler

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import cors from "cors";
 import express from "express";
 
 import connectDB from "./config/connectDb";
+import { errorHandler } from "./middlewares";
+import { CustomError } from "./utils";
 
 const app = express();
 
@@ -16,6 +18,13 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
+
+app.all("*", (req, _res, next): void => {
+  const error = new CustomError(`Route ${req.originalUrl} not found`, 404);
+  next(error);
+});
+
+app.use(errorHandler);
 
 // Database connection
 connectDB();
